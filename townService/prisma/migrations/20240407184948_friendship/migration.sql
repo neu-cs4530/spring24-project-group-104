eea775bd8d890646f7874d2682fb635b943d1e22
Satwik Misra <71944627+satm-24@users.noreply.github.com>
2024-04-08 12:49:09 -0400

    Update migration.sql

--- townService/prisma/migrations/20240407184948_friendship/migration.sql
+++ townService/prisma/migrations/20240407184948_friendship/migration.sql
@@ -6,8 +6,8 @@
   - Made the column `displayName` on table `User` required. This step will fail if there are existing NULL values in that column.
 
 */
--- AlterTable
-ALTER TABLE "ChatMessage" ADD COLUMN     "sid" TEXT NOT NULL;
+-- -- AlterTable
+-- ALTER TABLE "ChatMessage" ADD COLUMN     "sid" TEXT NOT NULL;
 
 -- AlterTable
 ALTER TABLE "User" ALTER COLUMN "displayName" SET NOT NULL;
